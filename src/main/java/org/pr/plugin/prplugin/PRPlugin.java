345642f229630691119d81121d8e3c236bf19c2c
FxFurEN <nikitostimoshenko@gmail.com>
2023-04-18 09:37:03 +0300

    review

---
+++ src/main/java/org/pr/plugin/prplugin/PRPlugin.java
@@ -0,0 +1,17 @@
+package org.pr.plugin.prplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PRPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
